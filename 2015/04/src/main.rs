249eeee7480b8de6b6ba1b3bb20a72087bb322e3
Mårten Kongstad <marten.kongstad@gmail.com>
2021-01-01 18:18:33 +0100

    AOC 2015 day 4
    Performance measurements from various iterations of development:
    
    Single threaded (crate md5):
    
      Time (mean ± σ):      2.495 s ±  0.039 s    [User: 2.492 s, System: 0.002 s]
      Range (min … max):    2.455 s …  2.590 s    10 runs
    
    Single threaded (crate rust-crypto):
    
      Time (mean ± σ):      2.007 s ±  0.019 s    [User: 2.007 s, System: 0.000 s]
      Range (min … max):    1.982 s …  2.050 s    10 runs
    
    Multi-threaded (using all cores):
    
      Time (mean ± σ):     399.3 ms ±   8.6 ms    [User: 3.065 s, System: 0.006 s]
      Range (min … max):   391.2 ms … 417.5 ms    10 runs
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

---
+++ 2015/04/src/main.rs
@@ -0,0 +1,107 @@
+use crypto::{digest::Digest, md5::Md5};
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
+
+fn main() {
+    let input = include_str!("input.txt").trim();
+
+    let answer = part_one(&input).expect("no solution for part one");
+    println!("part 1: {}", answer);
+
+    let answer = part_two(&input).expect("no solution for part two");
+    println!("part 2: {}", answer);
+}
+
+struct Context {
+    result: Option<usize>,
+    next: usize,
+    step_size: usize,
+}
+
+fn worker_thread<F>(secret_key: &str, verify_func: F, ctx: Arc<Mutex<Context>>)
+where
+    F: Fn(&[u8]) -> bool,
+{
+    let secret_key = secret_key.as_bytes();
+    let mut md5 = Md5::new();
+
+    loop {
+        let range = {
+            let mut ctx = ctx.lock().unwrap();
+            if ctx.result.is_some() || ctx.next >= usize::MAX - ctx.step_size {
+                return;
+            }
+            let range = ctx.next..=(ctx.next + ctx.step_size);
+            ctx.next += ctx.step_size;
+            range
+        };
+
+        for i in range {
+            md5.input(secret_key);
+            md5.input(i.to_string().as_bytes());
+            let mut hash = [0; 16];
+            md5.result(&mut hash);
+            if verify_func(&hash) {
+                let mut ctx = ctx.lock().unwrap();
+                ctx.result = Some(i);
+                return;
+            }
+            md5.reset();
+        }
+    }
+}
+
+fn find_number<F>(secret_key: &str, verify_func: F) -> Option<usize>
+where
+    F: Fn(&[u8]) -> bool + Copy + Send + 'static,
+{
+    let shared_ctx = Arc::new(Mutex::new(Context {
+        result: None,
+        next: 0,
+        step_size: 1000,
+    }));
+
+    let mut threads = Vec::new();
+
+    for _ in 0..num_cpus::get() {
+        let ctx = shared_ctx.clone();
+        let secret_key = secret_key.to_string();
+        threads.push(thread::spawn(move || {
+            worker_thread(&secret_key, verify_func, ctx)
+        }));
+    }
+
+    for t in threads {
+        t.join().unwrap();
+    }
+
+    let result = shared_ctx.lock().unwrap().result;
+    result
+}
+
+fn part_one(secret_key: &str) -> Option<usize> {
+    find_number(secret_key, |hash| {
+        debug_assert!(hash.len() == 16);
+        hash[0] == 0 && hash[1] == 0 && hash[2] <= 0x0f
+    })
+}
+
+fn part_two(secret_key: &str) -> Option<usize> {
+    find_number(secret_key, |hash| {
+        debug_assert!(hash.len() == 16);
+        hash[0] == 0 && hash[1] == 0 && hash[2] == 0
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        assert_eq!(part_one("abcdef"), Some(609043));
+        assert_eq!(part_one("pqrstuv"), Some(1048970));
+    }
+}

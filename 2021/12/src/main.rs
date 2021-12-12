f4325fa3567277bdf5e37693a3aa9842ff2b2bbc
Mårten Kongstad <marten.kongstad@gmail.com>
2021-12-12 20:19:30 +0100

    AOC 2021 day 12 (optimized version)
    To improve performance, switch
    
      - from String to u32 as node ID
      - from BTreeSet to Vec to track a node's edges
    
    Also hard-code the start and end nodes to always be NODE_START and
    NODE_END.
    
    Performance increase: runtime reduction 419.7 ms -> 51.8 ms.
    
      Previous version:
    
      $ hyperfine target/release/aoc-2021-12
      Benchmark #1: target/release/aoc-2021-12
        Time (mean ± σ):     419.7 ms ±   5.2 ms    [User: 390.4 ms, System: 29.1 ms]
        Range (min … max):   407.8 ms … 429.5 ms    10 runs
    
      This version:
    
      Benchmark #1: ../../target/release/aoc-2021-12
        Time (mean ± σ):      51.8 ms ±   2.5 ms    [User: 48.2 ms, System: 3.5 ms]
        Range (min … max):    49.2 ms …  57.5 ms    52 runs
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- 2021/12/src/main.rs
+++ 2021/12/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::BTreeMap;
 
 fn main() {
     let input = include_str!("input.txt");
@@ -17,46 +17,69 @@ enum Error {
     CyclicGraph,
 }
 
-type Graph = BTreeMap<String, BTreeSet<String>>;
+type Node = u32;
+const NODE_START: Node = 0;
+const NODE_END: Node = 1;
+type Graph = BTreeMap<Node, Vec<Node>>;
 
 fn parse_input(input: &str) -> Result<Graph, Error> {
+    let mut node_ids: BTreeMap<&str, Node> = BTreeMap::new();
+    node_ids.insert("start", NODE_START);
+    node_ids.insert("end", NODE_END);
+    let mut next_id = NODE_END + 1;
     let mut nodes = BTreeMap::new();
     for line in input.lines() {
+        macro_rules! parse_node {
+            ($split:ident, $next_id:ident) => {{
+                let node = $split.next().ok_or(Error::BadInput)?;
+                *node_ids.entry(node).or_insert_with(|| {
+                    // big caves are assigned an even number, small caves an odd number
+                    let odd_or_even = if node.chars().all(|ch| ch.is_ascii_uppercase()) {
+                        0
+                    } else {
+                        1
+                    };
+                    if next_id % 2 != odd_or_even {
+                        next_id += 1;
+                    } else {
+                        next_id += 2;
+                    }
+                    $next_id
+                })
+            }};
+        }
+
         let mut split = line.split('-');
-        let a = split.next().ok_or(Error::BadInput)?.to_string();
-        let b = split.next().ok_or(Error::BadInput)?.to_string();
+        let a = parse_node!(split, next_id);
+        let b = parse_node!(split, next_id);
         if split.next().is_some() {
             return Err(Error::BadInput);
         }
-        nodes
-            .entry(a.clone())
-            .or_insert_with(BTreeSet::new)
-            .insert(b.clone());
-        nodes.entry(b).or_insert_with(BTreeSet::new).insert(a);
+        nodes.entry(a).or_insert_with(Vec::new).push(b);
+        nodes.entry(b).or_insert_with(Vec::new).push(a);
     }
     Ok(nodes)
 }
 
-fn find_all_paths(
-    graph: &Graph,
-    start: &str,
-    end: &str,
-    allowance: usize,
-) -> Result<BTreeSet<Vec<String>>, Error> {
-    #[allow(clippy::ptr_arg)]
-    fn may_visit_again(current_path: &[String], node: &String) -> Option<usize> {
+fn find_all_paths(graph: &Graph, allowance: usize) -> Result<Vec<Vec<Node>>, Error> {
+    fn may_visit_again(current_path: &[Node], node: Node) -> Option<usize> {
+        // may never visit the start node again
+        if node == NODE_START {
+            return None;
+        }
+
         // big caves may be visited any number of times
-        if node.chars().all(|ch| ch.is_ascii_uppercase()) {
+        if node % 2 == 0 {
             return Some(0);
         }
 
         // all small caves may be visited once
-        if !current_path.contains(node) {
+        if !current_path.contains(&node) {
             return Some(0);
         }
 
         // exactly one small cave, except for start and end, may be visited twice
-        if node != "start" && node != "end" {
+        if node != NODE_END {
             return Some(1);
         }
 
@@ -65,53 +88,46 @@ fn find_all_paths(
 
     fn visit(
         graph: &Graph,
-        current_node: &str,
-        end_node: &str,
-        current_path: &[String],
+        current_path: &[Node],
         allowance: usize,
-    ) -> Result<BTreeSet<Vec<String>>, Error> {
+    ) -> Result<Vec<Vec<Node>>, Error> {
         if current_path.len() > 1000 {
             // max number 1000 arbitrarily chosen
             return Err(Error::CyclicGraph);
         }
+        let current_node = *current_path.iter().last().unwrap();
 
-        let mut set: BTreeSet<Vec<String>> = BTreeSet::new();
-        if current_node == end_node {
-            set.insert(current_path.to_vec());
+        let mut set: Vec<Vec<Node>> = Vec::new();
+        if current_node == NODE_END {
+            set.push(current_path.to_vec());
         } else {
             for (node, cost) in graph
-                .get(current_node)
+                .get(&current_node)
                 .ok_or(Error::NoSuchNode)?
                 .iter()
-                .map(|node| (node, may_visit_again(current_path, node)))
+                .map(|node| (node, may_visit_again(current_path, *node)))
                 .filter(|(_, cost)| cost.is_some() && cost.unwrap() <= allowance)
                 .map(|(node, cost)| (node, cost.unwrap()))
             {
                 let mut next_current_path = Vec::from_iter(current_path.iter().cloned());
-                next_current_path.push(node.to_string());
-                set.append(&mut visit(
-                    graph,
-                    node,
-                    end_node,
-                    &next_current_path,
-                    allowance - cost,
-                )?);
+                next_current_path.push(*node);
+                set.append(&mut visit(graph, &next_current_path, allowance - cost)?);
             }
         }
         Ok(set)
     }
 
-    visit(graph, start, end, &[start.to_string()], allowance)
+    visit(graph, &[NODE_START], allowance)
 }
 
 fn part_one(input: &str) -> Result<usize, Error> {
     let graph = parse_input(input)?;
-    Ok(find_all_paths(&graph, "start", "end", 0)?.len())
+    Ok(find_all_paths(&graph, 0)?.len())
 }
 
 fn part_two(input: &str) -> Result<usize, Error> {
     let graph = parse_input(input)?;
-    Ok(find_all_paths(&graph, "start", "end", 1)?.len())
+    Ok(find_all_paths(&graph, 1)?.len())
 }
 
 #[cfg(test)]
@@ -121,31 +137,6 @@ mod tests {
     const INPUT1: &str = include_str!("test-input1.txt");
     const INPUT2: &str = include_str!("test-input2.txt");
     const INPUT3: &str = include_str!("test-input3.txt");
-
-    #[test]
-    fn test_find_all_paths() {
-        const INPUT1: &str = include_str!("test-input1.txt");
-        let graph = parse_input(INPUT1).unwrap();
-        let paths = find_all_paths(&graph, "start", "end", 0).unwrap();
-        let mut paths: Vec<_> = paths.iter().collect();
-        paths.sort_unstable();
-        let paths: Vec<_> = paths.iter().map(|v| v.join(",")).collect();
-        assert_eq!(
-            paths,
-            vec![
-                "start,A,b,A,c,A,end",
-                "start,A,b,A,end",
-                "start,A,b,end",
-                "start,A,c,A,b,A,end",
-                "start,A,c,A,b,end",
-                "start,A,c,A,end",
-                "start,A,end",
-                "start,b,A,c,A,end",
-                "start,b,A,end",
-                "start,b,end",
-            ]
-        );
-    }
 
     #[test]
     fn test_part_one() {
